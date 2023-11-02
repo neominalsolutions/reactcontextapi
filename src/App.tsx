0aac5a4e887ae72fea24ee56fe61bc4350d9ddd7
neominalsolutions <neominal.solutions@gmail.com>
2023-11-02 09:19:48 +0300

    UseRoutes eklendi.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useRoutes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const routes = useRoutes([
+    {
+      path: '',
+      Component: null,
+      children: [{
+        path: '/deboucing',
+        Component: null
+      }, {
+        path: '/customHook',
+        Component: null
+      }, {
+        path: '/useReducer',
+        Component: null
+      }, {
+        path: '/useContext',
+        Component: null
+      }, {
+        path: '/reactFormsHook',
+        Component: null
+      }]
+    }
+  ]);
+
+  return routes;
+
 }
 
 export default App;
